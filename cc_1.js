71c1c99ca2518995ee5ec53ed446c2c4c6670222
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-15 19:20:30 -0400

    Initial logic for calculating tip using ternary operator.

--- cc_1.js
+++ cc_1.js
@@ -1,15 +1,15 @@
 
 // Initiate a bill value 
-let bill = 350;
+let bill = 275;
 
 // Declare the tip variable
 let tip;
 
 // Determine the tip amount
 if (bill >= 50 && bill <= 300){
-    tip = 0.2;
+    tip = 0.2*bill;
 } else{
-    tip = 0.15;
+    tip = 0.15*bill;
 }
 
 
