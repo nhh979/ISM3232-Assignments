d44bc833a1f2a5f60f926f90d1f11a5efef6d1aa
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-15 21:30:25 -0400

    Implement and populate arrays for bills, tips, and totals.

--- cc_1.js
+++ cc_1.js
@@ -33,6 +33,40 @@ function calcTip(bill){
 
 console.log(`The bill was \$100, the tip was \$${calcTip(100)}, and the total value \$${100 + calcTip(100)}.`);
 
+//--------------------------------------------------
+// Create a function to generate arrays for tips and totals
+  // take an array as an argument
+
+  function tips_totals_calc(bills){
+    let tips = bills.map(calcTip);       // Generate a tip array using the MAP method
+    let totals = [];                     // Initiate an empty array for totals
+
+    for (let i=0; i < bills.length; i++){    // Loop through the bills array and add it to the respective tip 
+        let total = bills[i] + tips[i];      // to calculate the total cost
+        totals.push(total);                  // add that total value to the totals array
+    }
+    return [tips, totals];            // return an array containing an array for tips and an array for totals
+}
+
+// Test case 1
+let bills_1 = [275, 40, 430];
+let [tips1, totals1] = tips_totals_calc(bills_1)
+
+console.log("----------------------------------------")
+console.log("Data set 1")
+console.log(`The bills are ${bills_1.join(', ')}.`);
+console.log(`The tips are ${tips1.join(', ')}.`);
+console.log(`The totals are ${totals1.join(', ')}.`);
+
+// Test case 2
+let bills_2 = [125, 555, 44];
+let [tips2, totals2] = tips_totals_calc(bills_2)
+
+console.log("----------------------------------------")
+console.log("Data set 2 ")
+console.log(`The bills are ${bills_2.join(', ')}.`);
+console.log(`The tips are ${tips2.join(', ')}.`);
+console.log(`The totals are ${totals2.join(', ')}.`);
 
 
 
